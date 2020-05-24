a4c770dbcb128ac62e3be02168dbd3233f2a2d49
wisnia74 <jakub.wisniowski74@gmail.com>
2020-05-24 16:03:03 +0200

    Configure eslint and nodemon

--- src/temp.ts
+++ src/temp.ts
@@ -1,4 +1,16 @@
-// replace with your path to openrct2.d.ts (usually it's C:\Users\<user>\Documents\OpenRCT2\bin\openrct2.d.ts)
+// replace <path> with your path to openrct2.d.ts (usually it's C:\Users\<user>\Documents\OpenRCT2\bin\openrct2.d.ts)
 // you will need to do that in each .ts file you'll want to use OpenRCT2 APIs in
 
 /// <reference path="<path>" />
+
+const main = () => {
+  console.log('Your plug-in has started!');
+};
+
+registerPlugin({
+  name: '<your_plugin_name>',
+  version: '1.0',
+  authors: ['<your_name>'],
+  type: '<type>',
+  main,
+});
