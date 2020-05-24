a9b0962a5b8275182ac389155f85e99ef1428474
wisnia74 <jakub.wisniowski74@gmail.com>
2020-05-24 15:14:41 +0200

    init

---
+++ src/temp.ts
@@ -0,0 +1,4 @@
+// replace with your path to openrct2.d.ts (usually it's C:\Users\<user>\Documents\OpenRCT2\bin\openrct2.d.ts)
+// you will need to do that in each .ts file you'll want to use OpenRCT2 APIs in
+
+/// <reference path="<path>" />
