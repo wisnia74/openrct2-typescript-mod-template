80cd10bc9a1cd84897d5ae5a9820453736b930bc
wisnia74 <41591175+wisnia74@users.noreply.github.com>
2020-05-27 20:31:01 +0200

    Update generators.js

--- utils/generators.js
+++ utils/generators.js
@@ -1,23 +1,47 @@
 exports.createTypeScriptConfig = (outDir) => ({
-  compilerOptions: {
-    target: 'es5',
-    module: 'commonjs',
-    declaration: true,
+  "compilerOptions": {
+    "target": 'es5',
+    "module": 'commonjs',
+    "declaration": true,
     outDir,
-    strict: true,
+    "strict": true,
   },
-  include: ['./src'],
-  exclude: ['node_modules', '**/__tests__/*'],
+  "include": ['./src'],
+  "exclude": ['node_modules', '**/__tests__/*'],
 });
 
 exports.createNodemonConfig = () => ({
-  events: {
-    restart: 'npm run build:develop',
+  "events": {
+    "restart": 'npm run build:develop',
   },
 });
 
 exports.createVsCodeConfig = () => ({
-  'typescript.tsdk': 'node_modules/typescript/lib',
+  "env": {
+      "browser": true,
+      "es6": true,
+      "node": true
+  },
+  "extends": [
+      "airbnb-base"
+  ],
+  "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly"
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "ecmaVersion": 11,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "@typescript-eslint"
+  ],
+  "rules": {
+      "no-undef": 0,
+      "spaced-comment": 0,
+      "max-len": ["error", { "code": 120 }]
+  }
 });
 
 exports.createTemplateModFile = (apiPath, modName, userName, modType) => `/// <reference path="${apiPath}" />
@@ -27,10 +51,10 @@ const main = () => {
 };
 
 registerPlugin({
-  name: '${modName}',
-  version: '1.0',
-  authors: ['${userName}'],
-  type: '${modType}',
+  "name": '${modName}',
+  "version": '1.0',
+  "authors": ['${userName}'],
+  "type": '${modType}',
   main,
 });
   
