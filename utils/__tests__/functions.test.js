02f8a3146c60e84f4396592a2fca1e63d91ea95d
wisnia74 <jakub.wisniowski74@gmail.com>
2020-05-31 17:06:30 +0200

    Adjust exec tests

--- utils/__tests__/functions.test.js
+++ utils/__tests__/functions.test.js
@@ -1,5 +1,7 @@
 describe('functions', () => {
   describe('exec', () => {
+    const opts = { stdio: undefined };
+
     it('should be an instance of Function', () => {
       const { exec } = require('../functions');
       expect(exec).toBeInstanceOf(Function);
@@ -8,13 +10,20 @@ describe('functions', () => {
     it('should run console commands', () => {
       const { exec } = require('../functions');
       expect(() => {
-        exec('true');
+        exec('echo', opts);
       }).not.toThrow();
     });
 
     it('should return undefined', () => {
-      const result = require('../functions').exec('true');
+      const result = require('../functions').exec('echo', opts);
       expect(result).toStrictEqual(undefined);
+    });
+
+    it('should throw an error if command is invalid', () => {
+      const { exec } = require('../functions');
+      expect(() => {
+        exec('invalid-command', opts);
+      }).toThrow();
     });
   });
 });
