fd1b9e8fe12b36cac77cedef2499e09e3b5e1149
wisnia74 <jakub.wisniowski74@gmail.com>
2022-06-01 21:48:05 +0200

    Fresh installation

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import main from './main';
+
+registerPlugin({
+  name: 'MOD_NAME',
+  version: '1.0',
+  authors: ['MOD_AUTHOR'],
+  type: 'local',
+  licence: 'MIT',
+  main,
+});
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { getHandymen, getMechanics, getSecurity, getEntertainers } from './helpers';
+
+const main = (): void => {
+  console.log(`Hello stranger! Your plug-in has started!`);
+
+  console.log(
+    `In your park, there are currently ${map.getAllEntities('guest').length + map.getAllEntities('staff').length} peeps`
+  );
+  console.log(`${map.getAllEntities('staff').length} of them is your staff.`);
+
+  console.log('Your staff consists of:');
+  console.log(`- ${getHandymen().length} handymen`);
+  console.log(`- ${getMechanics().length} mechanics`);
+  console.log(`- ${getSecurity().length} security`);
+  console.log(`- ${getEntertainers().length} entertainers`);
+};
+
+export default main;
