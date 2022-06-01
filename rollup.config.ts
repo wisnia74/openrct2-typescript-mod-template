ebf55f5ce99e367d96e54b235b2813d0d6af4d74
wisnia74 <jakub.wisniowski74@gmail.com>
2022-06-01 23:32:29 +0200

    Add Husky and more

---
+++ rollup.config.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import type { RollupOptions } from 'rollup';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+export default <RollupOptions>{
+  input: path.join(process.cwd(), 'src', 'index.ts'),
+  output: {
+    file: path.join(process.cwd(), 'dist', 'MOD_NAME.js'),
+    format: 'iife',
+  },
+  plugins: [
+    typescript(),
+    terser({
+      format: {
+        quote_style: 1,
+        wrap_iife: true,
+        preamble: '// Mod powered by https://github.com/wisnia74/openrct2-typescript-mod-template - MIT license',
+      },
+    }),
+  ],
+};
