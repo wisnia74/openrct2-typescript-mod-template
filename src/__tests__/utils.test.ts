e44cb903b834b04aa97d5cd39b7c1173f6c36437
wisnia74 <jakub.wisniowski74@gmail.com>
2022-06-07 19:33:06 +0200

    add setup file for mod tests

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,66 +1,7 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
+import { createMockStaff } from '~/testUtils';
 import * as utils from '../utils';
 
-const createMockStaff = (staffType: StaffType): Staff => ({
-  peepType: 'staff',
-  staffType,
-  colour: 0,
-  costume: 0,
-  orders: 0,
-  patrolArea: {
-    tiles: [],
-    clear: (): void => {},
-    add: (): void => {},
-    remove: (): void => {},
-    contains: (): boolean => true,
-  },
-  name: 'test',
-  destination: {
-    x: 0,
-    y: 0,
-  },
-  energy: 0,
-  energyTarget: 0,
-  getFlag: () => true,
-  setFlag: () => true,
-  id: 0,
-  type: 'staff',
-  x: 0,
-  y: 0,
-  z: 0,
-  remove: (): void => {},
-});
-
-function mockGetAllEntities(type: EntityType): Entity[];
-function mockGetAllEntities(type: 'peep'): Peep[];
-function mockGetAllEntities(type: 'guest'): Guest[];
-function mockGetAllEntities(type: 'staff'): Staff[];
-function mockGetAllEntities(type: 'car'): Car[];
-function mockGetAllEntities(type: 'litter'): Litter[];
-function mockGetAllEntities(): Entity[] {
-  return [
-    createMockStaff('handyman'),
-    createMockStaff('handyman'),
-    createMockStaff('handyman'),
-    createMockStaff('handyman'),
-    createMockStaff('handyman'),
-    createMockStaff('entertainer'),
-    createMockStaff('entertainer'),
-    createMockStaff('entertainer'),
-    createMockStaff('security'),
-    createMockStaff('security'),
-    createMockStaff('mechanic'),
-    createMockStaff('mechanic'),
-    createMockStaff('mechanic'),
-    createMockStaff('mechanic'),
-  ];
-}
-
 describe('utils', () => {
-  beforeAll(() => {
-    global.map = { getAllEntities: mockGetAllEntities } as GameMap;
-  });
-
   describe('isHandyman', () => {
     it('returns true if staff member is a handyman', () => {
       expect(utils.isHandyman(createMockStaff('handyman'))).toBe(true);
