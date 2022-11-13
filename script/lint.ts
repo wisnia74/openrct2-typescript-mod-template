bbdf6385ee4e7492f00c3d2e61b7e9b88221abe6
wisnia74 <jakub.wisniowski74@gmail.com>
2022-11-13 15:07:58 +0100

    update readme

--- script/lint.ts
+++ script/lint.ts
@@ -1,9 +1,10 @@
 import './registerCustomPaths';
 import { exec } from 'child_process';
-import { Logger } from '~/utils';
 import { ESLint } from 'eslint';
+import { Logger } from '~/utils';
 
 const isFixModeEnabled = !!process.argv.find((flag) => flag === '--fix');
+let hasAutoFixBeenRun = false;
 
 class LintRunner {
   private eslintInstance: ESLint;
@@ -22,10 +23,17 @@ class LintRunner {
 
     const results = await this.eslintInstance.lintFiles(['**/*.ts']);
 
-    if (isFixModeEnabled) {
-      this.logger.info('Found "--fix" flag, attempting to fix lint errors...');
+    if (results.some((result) => result.errorCount > 0) && isFixModeEnabled && !hasAutoFixBeenRun) {
+      this.logger.error('Found lint errros, attempting to auto-fix...');
+
       await ESLint.outputFixes(results);
+
+      this.logger.success('Successfully fixed auto-fixable lint errros, re-running lint...');
+
+      hasAutoFixBeenRun = true;
       exec('git add .');
+      await this.run();
+      return;
     }
 
     const formatter = await this.eslintInstance.loadFormatter('stylish');
